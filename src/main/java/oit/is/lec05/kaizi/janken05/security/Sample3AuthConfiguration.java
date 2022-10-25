4705f0c4a0e9185f13d2872ea98feb05641ba0cc
e1b20059 <e1b20059@oit.ac.jp>
2022-10-25 14:18:00 +0900

    認証処理

---
+++ src/main/java/oit/is/lec05/kaizi/janken05/security/Sample3AuthConfiguration.java
@@ -0,0 +1,69 @@
+package oit.is.lec05.kaizi.janken05.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class Sample3AuthConfiguration {
+
+  /**
+   * 認証処理に関する設定（誰がどのようなロールでログインできるか）
+   *
+   * @return
+   */
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    // このクラスの下部にあるPasswordEncoderを利用して，ユーザのビルダ（ログインするユーザを作成するオブジェクト）を作成する
+    UserBuilder users = User.builder();
+
+    // UserBuilder usersにユーザ名，パスワード，ロールを指定してbuildする
+    // このときパスワードはBCryptでハッシュ化されている．
+    // ハッシュ化されたパスワードを得るには，この授業のbashターミナルで下記のように末尾にユーザ名とパスワードを指定すると良い(要VPN)
+    // $ sshrun htpasswd -nbBC 10 user1 p@ss
+    // ロールを複数追加することもできる
+    UserDetails user1 = users
+        .username("user1")
+        .password("$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
+        .roles("USER", "MANAGER")
+        .build();
+    UserDetails user2 = users
+        .username("user2")
+        .password("$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
+        .roles("USER")
+        .build();
+    UserDetails admin = users
+        .username("admin")
+        .password("$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
+        .roles("ADMIN")
+        .build();
+    // $ sshrun htpasswd -nbBC 10 customer1 p@ss
+    UserDetails customer1 = users
+        .username("customer1")
+        .password("$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
+        .roles("CUSTOMER")
+        .build();
+    // $ sshrun htpasswd -nbBC 10 customer2 p@ss
+    UserDetails customer2 = users
+        .username("customer2")
+        .password("$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
+        .roles("CUSTOMER")
+        .build();
+    // $ sshrun htpasswd -nbBC 10 seller p@ss
+    UserDetails seller = users
+        .username("seller")
+        .password("$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
+        .roles("SELLER")
+        .build();
+
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
+    return new InMemoryUserDetailsManager(user1, user2, admin, customer1, customer2, seller);
+  }
+}
