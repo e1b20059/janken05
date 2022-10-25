d870bd1ad1f4cb6031a67df115e930413290b1fa
e1b20027 <e1b20027@oit.ac.jp>
2022-10-25 16:34:38 +0900

    step4,step5

--- src/main/java/oit/is/lec05/kaizi/janken05/Janken05Application.java
+++ src/main/java/oit/is/lec05/kaizi/janken05/Janken05Application.java
@@ -2,7 +2,11 @@ package oit.is.lec05.kaizi.janken05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Janken05Application {
 
