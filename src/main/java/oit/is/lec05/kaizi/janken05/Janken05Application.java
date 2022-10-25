8183060b0fe8441b4b518133ff3a3ada6509b0cd
e1b20059 <e1b20059@oit.ac.jp>
2022-10-25 14:07:36 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/lec05/kaizi/janken05/Janken05Application.java
@@ -0,0 +1,13 @@
+package oit.is.lec05.kaizi.janken05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken05Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec05/kaizi/janken05/Janken05ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec05.kaizi.janken05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
